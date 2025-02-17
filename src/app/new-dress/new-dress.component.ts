f84f7295bf66b9dfffbc10073f6fe7476ece2a5d
Suriyakannan <120772778+Suriyakannan@users.noreply.github.com>
2025-02-17 22:04:05 +0530

    created component setup

---
+++ src/app/new-dress/new-dress.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-new-dress',
+  standalone: true,
+  imports: [],
+  templateUrl: './new-dress.component.html',
+  styleUrl: './new-dress.component.scss'
+})
+export class NewDressComponent {
+
+}
